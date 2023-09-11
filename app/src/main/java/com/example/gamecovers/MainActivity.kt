8ce16e2b1a090327cebc8ed805dc9f6c6e5fd65d
Hernán Marrapodi <hernanmarra2002@gmail.com>
2023-09-11 19:26:14 -0300

    Finalize main activity and view

--- app/src/main/java/com/example/gamecovers/MainActivity.kt
+++ app/src/main/java/com/example/gamecovers/MainActivity.kt
@@ -3,26 +3,22 @@ package com.example.gamecovers
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -32,10 +28,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.gamecovers.repositories.GameRepository
@@ -72,15 +69,24 @@ fun GameCover(imageLink: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun GameInfo(title: String, developer: String, releaseDate: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(text = title)
-        Text(text = "$developer - $releaseDate")
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = title, fontWeight = FontWeight.Bold, fontSize = 30.sp)
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "$developer - $releaseDate", fontSize = 20.sp)
     }
 }
 
 @Composable
 fun NavButtons(onBackPress: () -> Unit, onNextPress: () -> Unit, modifier: Modifier = Modifier) {
-    Row {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Button(
             onClick = onBackPress,
             colors = ButtonDefaults.buttonColors(Color(0xFF2C002E), Color.White)
@@ -125,7 +131,7 @@ fun GameGallery() {
         ) {
             GameCover(game.permalink, modifier = Modifier.weight(6f))
             GameInfo(game.name, game.developer, game.releaseDate, modifier = Modifier.weight(1f))
-            NavButtons(decreaseValue, increaseValue, modifier = Modifier.weight(2f))
+            NavButtons(decreaseValue, increaseValue, modifier = Modifier.weight(1f))
         }
     }
 }
